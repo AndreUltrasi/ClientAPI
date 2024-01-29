177436225417f1ad9d1dc62b6d1125d47ea2cd72
caiosilvaferreira <caiosilvaferreira1@outlook.com.br>
2024-01-29 20:51:53 -0300

    implementado propriedades.

--- Core/Domain/Address.cs
+++ Core/Domain/Address.cs
@@ -3,5 +3,25 @@
     public class Address
     {
 
+
+        public int Guid { get; set; }
+
+        public bool Active { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModificationDate { get; set; }
+
+        public string Addresss { get; set; }
+
+        public string Number { get; set; }
+
+        public string Complement { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
     }
 }
--- Core/Domain/Client.cs
+++ Core/Domain/Client.cs
@@ -2,6 +2,23 @@
 {
     public class Client
     {
+        public Guid Id { get; set; }
+
+        public bool Active { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModificationDate { get; set; }
+
+        public string Name { get; set; }
+
+        public int PersonType { get; set; }
+
+        public string Age { get; set; }
+
+        public int Gender { get; set; }
+
+        public Guid AdressId { get; set; }
 
     }
 }
