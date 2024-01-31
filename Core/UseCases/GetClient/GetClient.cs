b780ae05d64fec6bbf42701e1285d4a37066333c
caiosilvaferreira <caiosilvaferreira1@outlook.com.br>
2024-01-31 12:28:47 -0300

    colocado prop no domain e incluido a documentação.

--- Core/UseCases/GetClient/GetClient.cs
+++ Core/UseCases/GetClient/GetClient.cs
@@ -4,14 +4,14 @@ using Core.UseCases.GetClient.Boundaries;
 //Core lida com regras de negocio
 namespace Core.UseCases.GetClient
 {
-    public class GetClient : IGetClient
-    {
-        public async Task<Client> Handle(GetClientInput input)
-        {
+    //public class GetClient : IGetClient
+    //{
+    //    public async Task<Client> Handle(GetClientInput input)
+    //    {
 
-        }
+    //    }
 
 
 
-    }
+    //}
 }
