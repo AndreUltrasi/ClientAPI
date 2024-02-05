93ffaa3379b735d196e2f7427db1f3e13d64a911
caiosilvaferreira <caiosilvaferreira1@outlook.com.br>
2024-02-05 18:18:56 -0300

    feito algumas alteções no infra

--- Infra/Repositories/ClientRepository.cs
+++ Infra/Repositories/ClientRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Core;
 using Core.Domain;
 using Infra.Mappers;
+using Microsoft.EntityFrameworkCore;
 
 //Camada responsável por gravar/atualizar/deletar informações no banco, e responsável por acessar apis
 namespace Infra.Repositories
@@ -36,5 +37,14 @@ namespace Infra.Repositories
 
             return client;
         }
+
     }
+
+    public class BlogDataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options
+            .UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$ ; TrustServerCertificate=True");
+    }
+
 }
