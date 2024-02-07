abcc16803fe31634d8c325ddd7e51a91477c315d
André Augusto <dede0max@hotmail.com>
2024-02-07 00:52:53 -0300

    Removendo codigo inutilizado

--- Infra/Repositories/ClientRepository.cs
+++ Infra/Repositories/ClientRepository.cs
@@ -30,12 +30,4 @@ namespace Infra.Repositories
         }
 
     }
-
-    public class BlogDataContext : DbContext
-    {
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options
-            .UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$ ; TrustServerCertificate=True");
-    }
-
 }
